b3470563c1e1da64cb346f8f3add1316639c78c5
yuki1031-hub <hishikawa1031@gmail.com>
2026-02-04 17:57:36 +0900

    Switch to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Diagnosis from "./Diagnosis";
 import AdChatbot from "./AdChatbot";
