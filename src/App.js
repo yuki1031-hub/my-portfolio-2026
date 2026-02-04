f13d7c6e7878aff2f4330b4d5761097f8f86a7a1
yuki1031-hub <hishikawa1031@gmail.com>
2026-02-04 17:26:11 +0900

    Add routing for direct links

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React from "react";
-// ページ切り替え機能（ルーター）を読み込み
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-// 各ページの部品を読み込み
 import Home from "./Home";
 import Diagnosis from "./Diagnosis";
 import AdChatbot from "./AdChatbot";
@@ -12,13 +9,8 @@ export default function App() {
   return (
     <Router>
       <Routes>
-        {/* URLが「/」のときは Home を表示 */}
         <Route path="/" element={<Home />} />
-        
-        {/* URLが「/diagnosis」のときは 性格診断だけ を表示 */}
         <Route path="/diagnosis" element={<Diagnosis />} />
-
-        {/* URLが「/ad-chatbot」のときは 広告ボットだけ を表示 */}
         <Route path="/ad-chatbot" element={<AdChatbot />} />
       </Routes>
     </Router>
